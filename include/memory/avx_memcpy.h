c9941087999a9379a8b529e8edc507c8bae79e08
Chris <>
2024-05-01 03:48:42 -0500

    ok

--- include/memory/avx_memcpy.h
+++ include/memory/avx_memcpy.h
@@ -21,13 +21,16 @@ static inline void avx_memcpy(u8* dst,
                               const u8* src,
                               u64 size)
 {
-  if (size < 64) {
-    memcpy(dst, src, size);
+  if (size < 32) {
+    for (u32 b = 0; b < size; ++b) {
+      dst[b] = src[b];
+    }
     return;
   }
   const I8* src_v = (const I8*)src;
   I8* dst_v = (I8*)dst;
-  if (DST_AVX_ALIGNED && SRC_AVX_ALIGNED) {
+  if ((DST_AVX_ALIGNED || Math::is_aligned<32>(dst)) && 
+      (SRC_AVX_ALIGNED || Math::is_aligned<32>(src))) {
     u32 register_count = (size >> 5);
     if (size <= MEGABYTE) {
       for (u32 r = 0; r < register_count; r += 2) {
@@ -37,7 +40,6 @@ static inline void avx_memcpy(u8* dst,
         I8_STORE(dst_v + 1, b);
         dst_v += 2;
         src_v += 2;
-        size -= 64;
       }
     } else {
       for (u32 r = 0; r < register_count; r += 2) {
@@ -47,7 +49,6 @@ static inline void avx_memcpy(u8* dst,
         I8_STORE_NOCACHE(dst_v + 1, b);
         dst_v += 2;
         src_v += 2;
-        size -= 64;
       }
       FENCE();
     }
@@ -56,8 +57,7 @@ static inline void avx_memcpy(u8* dst,
     I8_STOREU(dst_v, I8_LOADU(src_v));
     dst_v = (I8*)(dst + padding);
     src_v = (I8*)(src + padding);
-    size -= padding;
-    u32 register_count = (size >> 5);
+    u32 register_count = ((size - padding) >> 5);
     if (size <= MEGABYTE) {
       for (u32 r = 0; r < register_count; r += 2) {
         I8 a = I8_LOADU(src_v);
@@ -66,7 +66,6 @@ static inline void avx_memcpy(u8* dst,
         I8_STORE(dst_v + 1, b);
         dst_v += 2;
         src_v += 2;
-        size -= 64;
       }
     } else {
       for (u32 r = 0; r < register_count; r += 2) {
@@ -76,10 +75,15 @@ static inline void avx_memcpy(u8* dst,
         I8_STORE_NOCACHE(dst_v + 1, b);
         dst_v += 2;
         src_v += 2;
-        size -= 64;
       }
       FENCE();
     }
   }
+  dst_v = (I8*)(dst + size - 64);
+  src_v = (I8*)(src + size - 64);
+  I8 a = I8_LOADU(src_v);
+  I8 b = I8_LOADU(src_v + 1);
+  I8_STOREU(dst_v, a);
+  I8_STOREU(dst_v + 1, b);
 }
 }
