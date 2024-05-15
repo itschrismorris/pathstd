4fca2d379a26b0e7256920422d3db6952cce8046
Chris <>
2024-05-15 12:38:47 -0500

    ok

--- include/string/format_type.h
+++ include/string/format_type.h
@@ -17,23 +17,23 @@
     total_size += copy_size; \
   }
 
-#define __DECORATE_INT(IN, DECORATE, DECORATE_SIZE) \
+#define __DECORATE_INT(ARG, DECORATE, DECORATE_SIZE) \
   { \
     u64 copy_size = Math::min(string_capacity - 1 - total_size, DECORATE_SIZE); \
     Memory::memcpy(&string_out[total_size], &decorate[DECORATE], copy_size); \
     total_size += copy_size; \
-    utf8* buffer_str = String::from_int(IN, buffer, &conversion_size); \
+    utf8* buffer_str = String::from_int(ARG, buffer, &conversion_size); \
     copy_size = Math::min(string_capacity - 1 - total_size, conversion_size); \
     Memory::memcpy(&string_out[total_size], buffer_str, copy_size); \
     total_size += copy_size; \
   }
 
-#define __DECORATE_FLOAT(IN, DECORATE, DECORATE_SIZE) \
+#define __DECORATE_FLOAT(ARG, DECORATE, DECORATE_SIZE) \
   { \
     u64 copy_size = Math::min(string_capacity - 1 - total_size, DECORATE_SIZE); \
     Memory::memcpy(&string_out[total_size], &decorate[DECORATE], copy_size); \
     total_size += copy_size; \
-    utf8* buffer_str = String::from_float(IN, buffer, &conversion_size); \
+    utf8* buffer_str = String::from_float(ARG, buffer, &conversion_size); \
     copy_size = Math::min(string_capacity - 1 - total_size, conversion_size); \
     Memory::memcpy(&string_out[total_size], buffer_str, copy_size); \
     total_size += copy_size; \
@@ -44,26 +44,26 @@ namespace Pathlib::String {
 template <typename T>
 static inline u64 format_type(utf8* string_out,
                               u64 string_capacity,
-                              const T in)
+                              const T arg)
 {
   static_assert(!SAME_TYPE(T, const char*), "String literals must be prepended with 'u8' for utf-8 encoding: 'u8\"Hello world!\"'");
   static_assert(!SAME_TYPE(T, char*), "Replace string usages of char with utf8, for utf-8 encoding.");
   if constexpr (SAME_TYPE(T, const utf8*) || SAME_TYPE(T, utf8*)) {
-    u64 copy_size = Math::min(string_capacity - 1, strlen(in));
-    Memory::memcpy(string_out, in, copy_size);
+    u64 copy_size = Math::min(string_capacity - 1, strlen(arg));
+    Memory::memcpy(string_out, arg, copy_size);
     return copy_size;
   } else if constexpr (SAME_TYPE(T, i8) || SAME_TYPE(T, i16) || SAME_TYPE(T, i32) || SAME_TYPE(T, i64) ||
                        SAME_TYPE(T, u8) || SAME_TYPE(T, u16) || SAME_TYPE(T, u32) || SAME_TYPE(T, u64)) {
     utf8 buffer[32];
     u64 size;
-    utf8* buffer_str = String::from_int(in, buffer, &size);
+    utf8* buffer_str = String::from_int(arg, buffer, &size);
     u64 copy_size = Math::min(string_capacity - 1, size);
     Memory::memcpy(string_out, buffer_str, copy_size);
     return copy_size;
   } else if constexpr (SAME_TYPE(T, f32) || SAME_TYPE(T, f64)) {
     utf8 buffer[32];
     u64 size;
-    utf8* buffer_str = String::from_float(in, buffer, &size);
+    utf8* buffer_str = String::from_float(arg, buffer, &size);
     u64 copy_size = Math::min(string_capacity - 1, size);
     Memory::memcpy(string_out, buffer_str, copy_size);
     return copy_size;
@@ -72,8 +72,8 @@ static inline u64 format_type(utf8* string_out,
     utf8 buffer[32];
     u64 conversion_size;
     u64 total_size = 0;
-    __DECORATE_INT(in.x, 0LLU, 5LLU);
-    __DECORATE_INT(in.y, 5LLU, 5LLU);
+    __DECORATE_INT(arg.x, 0LLU, 5LLU);
+    __DECORATE_INT(arg.y, 5LLU, 5LLU);
     __DECORATE(9LLU, 2LLU);
     return total_size;
   } else if constexpr (SAME_TYPE(T, i32_3) || SAME_TYPE(T, u32_3))  {
@@ -81,9 +81,9 @@ static inline u64 format_type(utf8* string_out,
     utf8 buffer[32];
     u64 conversion_size;
     u64 total_size = 0;
-    __DECORATE_INT(in.x, 0LLU, 5LLU);
-    __DECORATE_INT(in.y, 5LLU, 5LLU);
-    __DECORATE_INT(in.z, 10LLU, 5LLU);
+    __DECORATE_INT(arg.x, 0LLU, 5LLU);
+    __DECORATE_INT(arg.y, 5LLU, 5LLU);
+    __DECORATE_INT(arg.z, 10LLU, 5LLU);
     __DECORATE(14LLU, 2LLU);
     return total_size;
   } else if constexpr (SAME_TYPE(T, i32_4) || SAME_TYPE(T, u32_4))  {
@@ -91,10 +91,10 @@ static inline u64 format_type(utf8* string_out,
     utf8 buffer[32];
     u64 conversion_size;
     u64 total_size = 0;
-    __DECORATE_INT(in.x, 0LLU, 5LLU);
-    __DECORATE_INT(in.y, 5LLU, 5LLU);
-    __DECORATE_INT(in.z, 10LLU, 5LLU);
-    __DECORATE_INT(in.w, 15LLU, 5LLU);
+    __DECORATE_INT(arg.x, 0LLU, 5LLU);
+    __DECORATE_INT(arg.y, 5LLU, 5LLU);
+    __DECORATE_INT(arg.z, 10LLU, 5LLU);
+    __DECORATE_INT(arg.w, 15LLU, 5LLU);
     __DECORATE(19LLU, 2LLU);
     return total_size;
   } else if constexpr (SAME_TYPE(T, i32_8) || SAME_TYPE(T, u32_8))  {
@@ -102,15 +102,15 @@ static inline u64 format_type(utf8* string_out,
     utf8 buffer[32];
     u64 conversion_size;
     u64 total_size = 0;
-    __DECORATE_INT(in.lo.x, 0LLU, 11LLU);
-    __DECORATE_INT(in.lo.y, 11LLU, 5LLU);
-    __DECORATE_INT(in.lo.z, 16LLU, 5LLU);
-    __DECORATE_INT(in.lo.w, 21LLU, 5LLU);
+    __DECORATE_INT(arg.lo.x, 0LLU, 11LLU);
+    __DECORATE_INT(arg.lo.y, 11LLU, 5LLU);
+    __DECORATE_INT(arg.lo.z, 16LLU, 5LLU);
+    __DECORATE_INT(arg.lo.w, 21LLU, 5LLU);
     __DECORATE(25LLU, 8LLU);
-    __DECORATE_INT(in.hi.x, 6LLU, 5LLU);
-    __DECORATE_INT(in.hi.y, 11LLU, 5LLU);
-    __DECORATE_INT(in.hi.z, 16LLU, 5LLU);
-    __DECORATE_INT(in.hi.w, 21LLU, 5LLU);
+    __DECORATE_INT(arg.hi.x, 6LLU, 5LLU);
+    __DECORATE_INT(arg.hi.y, 11LLU, 5LLU);
+    __DECORATE_INT(arg.hi.z, 16LLU, 5LLU);
+    __DECORATE_INT(arg.hi.w, 21LLU, 5LLU);
     __DECORATE(25LLU, 2LLU);
     return total_size;
   } else if constexpr (SAME_TYPE(T, f32_2))  {
@@ -118,8 +118,8 @@ static inline u64 format_type(utf8* string_out,
     utf8 buffer[32];
     u64 conversion_size;
     u64 total_size = 0;
-    __DECORATE_FLOAT(in.x, 0LLU, 5LLU);
-    __DECORATE_FLOAT(in.y, 5LLU, 5LLU);
+    __DECORATE_FLOAT(arg.x, 0LLU, 5LLU);
+    __DECORATE_FLOAT(arg.y, 5LLU, 5LLU);
     __DECORATE(9LLU, 2LLU);
     return total_size;
 
@@ -128,9 +128,9 @@ static inline u64 format_type(utf8* string_out,
     utf8 buffer[32];
     u64 conversion_size;
     u64 total_size = 0;
-    __DECORATE_FLOAT(in.x, 0LLU, 5LLU);
-    __DECORATE_FLOAT(in.y, 5LLU, 5LLU);
-    __DECORATE_FLOAT(in.z, 10LLU, 5LLU);
+    __DECORATE_FLOAT(arg.x, 0LLU, 5LLU);
+    __DECORATE_FLOAT(arg.y, 5LLU, 5LLU);
+    __DECORATE_FLOAT(arg.z, 10LLU, 5LLU);
     __DECORATE(14LLU, 2LLU);
     return total_size;
 
@@ -139,10 +139,10 @@ static inline u64 format_type(utf8* string_out,
     utf8 buffer[32];
     u64 conversion_size;
     u64 total_size = 0;
-    __DECORATE_FLOAT(in.x, 0LLU, 5LLU);
-    __DECORATE_FLOAT(in.y, 5LLU, 5LLU);
-    __DECORATE_FLOAT(in.z, 10LLU, 5LLU);
-    __DECORATE_FLOAT(in.w, 15LLU, 5LLU);
+    __DECORATE_FLOAT(arg.x, 0LLU, 5LLU);
+    __DECORATE_FLOAT(arg.y, 5LLU, 5LLU);
+    __DECORATE_FLOAT(arg.z, 10LLU, 5LLU);
+    __DECORATE_FLOAT(arg.w, 15LLU, 5LLU);
     __DECORATE(19LLU, 2LLU);
     return total_size;
 
@@ -151,15 +151,15 @@ static inline u64 format_type(utf8* string_out,
     utf8 buffer[32];
     u64 conversion_size;
     u64 total_size = 0;
-    __DECORATE_FLOAT(in.lo.x, 0LLU, 11LLU);
-    __DECORATE_FLOAT(in.lo.y, 11LLU, 5LLU);
-    __DECORATE_FLOAT(in.lo.z, 16LLU, 5LLU);
-    __DECORATE_FLOAT(in.lo.w, 21LLU, 5LLU);
+    __DECORATE_FLOAT(arg.lo.x, 0LLU, 11LLU);
+    __DECORATE_FLOAT(arg.lo.y, 11LLU, 5LLU);
+    __DECORATE_FLOAT(arg.lo.z, 16LLU, 5LLU);
+    __DECORATE_FLOAT(arg.lo.w, 21LLU, 5LLU);
     __DECORATE(25LLU, 8LLU);
-    __DECORATE_FLOAT(in.hi.x, 6LLU, 5LLU);
-    __DECORATE_FLOAT(in.hi.y, 11LLU, 5LLU);
-    __DECORATE_FLOAT(in.hi.z, 16LLU, 5LLU);
-    __DECORATE_FLOAT(in.hi.w, 21LLU, 5LLU);
+    __DECORATE_FLOAT(arg.hi.x, 6LLU, 5LLU);
+    __DECORATE_FLOAT(arg.hi.y, 11LLU, 5LLU);
+    __DECORATE_FLOAT(arg.hi.z, 16LLU, 5LLU);
+    __DECORATE_FLOAT(arg.hi.w, 21LLU, 5LLU);
     __DECORATE(25LLU, 2LLU);
     __DECORATE(25LLU, 2LLU);
     return total_size;
